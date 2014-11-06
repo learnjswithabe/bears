7b7de4e3a2352eebc5cfb61b636538b4de4eec32
Austin Lawless <austin.lawless@bancvue.com>
2014-11-06 13:09:09 -0600

    added get by id

--- server.js
+++ server.js
@@ -66,6 +66,20 @@ router.route('/bears')
 		});
 	});
 
+// on routes that end in /bears/:beard_id
+// ----------------------------------------------
+router.route('/bears/:bear_id')
+
+	// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+	.get(function(req, res) {
+		Bear.findById(req.params.bear_id, function(err, bear) {
+			if (err) {
+				res.send(err);
+			}
+			res.json(bear);
+		});
+	});
+
 
 // REGISTER OUR ROUTES --------------------------
 // all of our routes will be prefixed with /api
